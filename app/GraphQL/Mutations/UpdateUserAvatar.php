68a2d5fc12c4d4bcb92f743f0e1e17b53fa64ff6
barron <wewececetw@gmail.com>
2023-10-08 22:31:11 +0800

    feat: 會員圖片上傳

---
+++ app/GraphQL/Mutations/UpdateUserAvatar.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\User;
+
+final class UpdateUserAvatar
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+        $file = $args['image'];
+        $path = $file->storePublicly('public/uploads');
+        $user = User::find($args['id']);
+        $user->avatar = $path;
+        $user->save();
+        return $user;
+    }
+}
